7c733eb1557e72f8869cb2b50469882488800377
IlyaPyatkin <baldurmods@gmail.com>
2016-07-18 03:04:37 +0300

    Used GoogleHandler in Main as an example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is just a test");
+        GoogleHandler webHandler = GoogleHandler.getHandler();
+        Set<String> result = webHandler.getData("ehotka", "vk.com");
+        for(String str: result)
+            System.out.println(str);
     }
 }
